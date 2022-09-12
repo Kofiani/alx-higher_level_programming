07928d1c2aef6c612a7f7220251d11277411958c
Kofiani <samuelannie.k@gmail.com>
2022-09-12 20:30:42 +0100

    List Division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,4 +17,4 @@ def list_division(my_list_1, my_list_2, list_length):
             div_list = 0
         finally:
             new_list.append(div_list)
-        return (new_list)
+    return (new_list)
