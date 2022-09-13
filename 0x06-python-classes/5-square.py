5d76fb476531fa0f4342555b9182be8908d55d04
Kofiani <samuelannie.k@gmail.com>
2022-09-13 19:54:30 +0100

    Added task-5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' add print method to class '''
+
+
+class Square:
+    ''' Add my_print method to class '''
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(size):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print(" ")
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
