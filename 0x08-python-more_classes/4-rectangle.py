7fee702e47f975f0a1fad80f4868b2aff06f962a
Kofiani <samuelannie.k@gmail.com>
2022-09-19 19:51:29 +0100

    created fourth rectangle class

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -55,27 +55,3 @@ class Rectangle:
     def __repr__(self):
         rect = "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
         return (rect)
-
-my_rectangle = Rectangle(2, 4)
-print(str(my_rectangle))
-print("--")
-print(my_rectangle)
-print("--")
-print(repr(my_rectangle))
-print("--")
-print(hex(id(my_rectangle)))
-print("--")
-
-# create new instance based on representation
-new_rectangle = eval(repr(my_rectangle))
-print(str(new_rectangle))
-print("--")
-print(new_rectangle)
-print("--")
-print(repr(new_rectangle))
-print("--")
-print(hex(id(new_rectangle)))
-print("--")
-
-print(new_rectangle is my_rectangle)
-print(type(new_rectangle) is type(my_rectangle))
