8565cc51aebe6316dd2316fb8fed521fe961878b
Kofiani <samuelannie.k@gmail.com>
2022-09-19 17:15:55 +0100

    created an first rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value):
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
@@ -30,7 +30,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value):
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value < 0:
             raise ValueError('height must >= 0')
