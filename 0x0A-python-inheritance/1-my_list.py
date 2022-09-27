0631907a56f0cdcb514e44912efdedb9ca22d127
Kofiani <samuelannie.k@gmail.com>
2022-09-27 01:22:53 +0100

    Fixed bug

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-''' create MyList to inherit from list '''
+"""
+contains the MyList class
+"""
 
 
 class MyList(list):
-    ''' MyList is a subclass of list object '''
+    """a subclass of list"""
 
     def __init__(self):
+        """initializes the object"""
         super().__init__()
 
     def print_sorted(self):
