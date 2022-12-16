f275c443a738d0a506578f92dcf52dbdeb28b7ab
Kofiani <samuelannie.k@gmail.com>
2022-12-16 19:49:16 +0100

    test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
         content = r.read()
         print('Body response:')
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
+        print(f"\t - type: {type(content)}")
+        print(f"\t - content: {content}")
+        print(f"\t - utf8 content: {content.decode('utf-8')}")
